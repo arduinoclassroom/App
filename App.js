63d93dc940c9f574ae5de38cd419e04866ba07a1
Arduino classroom <oshandissanayaka090@gmail.com>
2023-08-17 21:19:21 +0530

    test

--- App.js
+++ App.js
@@ -47,7 +47,6 @@ export default function App() {
   const [status6, setStatus6] = useState(false);
 
   const db = getDatabase();
-  //Hi fireds
 
   useEffect(() => {
     const starCountRef = ref(db, "swiches/");
